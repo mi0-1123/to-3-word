03f647338510dc4f69ebcf7f408bb127b5b70e1f
mi0-1123 <si2.o.mjt@gmail.com>
2019-01-28 22:34:09 +0900

    fix/add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <navbar style="margin-bottom:10px"/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
-  name: "App"
+  name: "App",
+  components: { Navbar }
 };
 </script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: "Header",
+  name: "Navbar",
   data() {
     return {};
   }
